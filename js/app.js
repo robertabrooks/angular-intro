4367c2d7b1384f5ce3b3133a17b28cbeb5433dd6
robertabrooks <robertb1@uw.edu>
2014-11-04 12:15:17 -0800

    class work:

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-/*
+/*0
     app.js: main application script
     this is an Angular application
  */
 
 "use strict";
+
+angular.module('AngularDemo', [])
+    .controller('HelloController', function($scope){
+        $scope.person = {
+            firstName: null,
+            lastName: null
+        };
+    })
+    .controller('MoviesController', function($scope) {
+       $scope.movies = movies;
+       $scope.sortCol = 'rank';
+       $scope.sortBy = function(sortCol) {
+          if ($scope.sortCol == sortCol) {
+              $scope.sortReverse = !$scope.sortReverse;
+          } else {
+              $scope.sortCol = sortCol;
+              $scope.sortReverse = false;
+          }
+       };
+       $scope.isSortedBy = function(colName) {
+           return colName == $scope.sortCol;
+       }
+    });
 
